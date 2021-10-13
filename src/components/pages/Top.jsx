693bc5fd21922b7e42ecee368c153b6ec368d562
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-13 10:31:59 +0900

    add button and link in Top.jsx

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,20 @@
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import { SecondaryButton } from '../atoms/button/SecondaryButton';
 
 export const Top = () => {
+  const history = useHistory();
+  // const onClickAdmin = () => history.push({pathname: "/users", isAdmin: true});
+  const onClickAdmin = () => history.push({pathname: "/users", state: { isAdmin: true }});
+  const onClickGeneral = () => history.push({pathname: "/users", state: { isAdmin: false }});
+
   return (
     <SContainer>
       <h2>This is TOP page!</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
