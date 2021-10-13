664f02b769957576074ba91ab06f69d4d0544ab3
Hirohito Terauchi <tera9319@yahoo.co.jp>
2021-10-13 14:59:49 +0900

    add memo

--- src/components/moreculres/SearchInput.jsx
+++ src/components/moreculres/SearchInput.jsx
@@ -1,11 +1,12 @@
+import { memo } from 'react';
 import { PrimaryButton } from '../atoms/button/PrimaryButton';
 import { Input } from '../atoms/input/Input';
 
-export const SearchInput = () => {
+export const SearchInput = memo(() => {
   return (
     <div>
       <Input placeholder="Input Anything"></Input>
       <PrimaryButton>Search</PrimaryButton>
     </div>
   );
-};
+});
--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,8 +1,9 @@
+import { memo } from 'react';
 import styled from 'styled-components';
 import { Card } from '../../atoms/card/Card';
 import { UserIconWithName } from '../../moreculres/user/UserIconWithName';
 
-export const UserCard = (props) => {
+export const UserCard = memo((props) => {
   const user = props.user;
 
   return (
@@ -20,7 +21,7 @@ export const UserCard = (props) => {
       </SDl>
     </Card>
   );
-};
+});
 
 const SDl = styled.dl`
   margin-bottom: 0;
